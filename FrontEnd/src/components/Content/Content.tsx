7c851f3421638083713d286aecfae51f968916f9
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-03 20:28:02 +0530

    Update Content

--- FrontEnd/src/components/Content/Content.tsx
+++ FrontEnd/src/components/Content/Content.tsx
@@ -4,6 +4,7 @@ import Gallery from "../../pages/Gallery";
 import Blog from "../../pages/Blog";
 import Contact from "../../pages/Contact";
 import Places from "../../pages/Places";
+import Login from "../../pages/Login";
 
 export default function Content() {
   return (
@@ -14,6 +15,7 @@ export default function Content() {
       <Route path="/gallery" element={<Gallery />}></Route>
       <Route path="/blog" element={<Blog />}></Route>
       <Route path="/contact" element={<Contact />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 }
