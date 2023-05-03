139d381c864d8a6b31640187fd84a03c8c616cde
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-03 16:37:05 +0530

    Update Content

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Content from "./components/Content/Content";
-import Header from "./components/Header/Header";
 
 export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="min-h-screen">
-      {/* <Content/> */}
-      <Header/>
+      <Content/>
       </div>
     );
   }
