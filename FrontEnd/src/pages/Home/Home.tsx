4f6f099a1072b654939f309557035fb972799c74
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-03 21:23:23 +0530

    Update Home content to img

--- FrontEnd/src/pages/Home/Home.tsx
+++ FrontEnd/src/pages/Home/Home.tsx
@@ -1,13 +1,26 @@
-import React from "react";
 import Header from "../../components/Header";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import background1Image from "../../assets/img/seaHotels.jpg";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div>
-        <h1>Hi</h1>
-      </div>
+      <Card sx={{ mt: 1 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          sx={{ height: 380 }}
+          image={background1Image}
+        />
+      </Card>
     </>
   );
 }
