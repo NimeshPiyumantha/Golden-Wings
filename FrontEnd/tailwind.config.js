9127573605e9e11cc92584264219777507284e90
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-03 11:47:47 +0530

    Add screens  to tailwind.config.file

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: [],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
         accent: {
