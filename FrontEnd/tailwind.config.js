ed0a929cdf50a7a125b24e8f2692da4ae5ba9239
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-03 11:06:11 +0530

    Add yellow color to tailwind.config.file

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          yellow: {
+            50: "#f6e58d",
+            100: "#fed330",
+            200: "#f7b731",
+          },
+         
+        },
+      },
+   
+    },
   },
   plugins: [],
-}
-
+};
