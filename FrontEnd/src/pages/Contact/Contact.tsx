686c3d05a3bf142d5a2409b38e528b6c2a235d15
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-04 17:27:06 +0530

    Update Content

--- FrontEnd/src/pages/Contact/Contact.tsx
+++ FrontEnd/src/pages/Contact/Contact.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 export default function Contact() {
   return (
     <>
     <Header/>
+    
     <div>Contact</div>
+
+    <Footer/>
     </>
   )
 }
