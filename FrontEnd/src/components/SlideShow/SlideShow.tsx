c9d41c6fb322c659831687d1ac6b38dc640eb408
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-04 09:28:12 +0530

    Update Content

---
+++ FrontEnd/src/components/SlideShow/SlideShow.tsx
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./SlideShow.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function SlideShow() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- FrontEnd/src/pages/Home/Home.tsx
+++ FrontEnd/src/pages/Home/Home.tsx
@@ -1,12 +1,5 @@
 import Header from "../../components/Header";
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-} from "@mui/material";
+import { Card, CardMedia } from "@mui/material";
 import background1Image from "../../assets/img/seaHotels.jpg";
 
 export default function Home() {
@@ -14,12 +7,7 @@ export default function Home() {
     <>
       <Header />
       <Card sx={{ mt: 1 }}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          sx={{ height: 380 }}
-          image={background1Image}
-        />
+        
       </Card>
     </>
   );
