b076e84fd9986af4eed13d20d03b62559ba7743d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-04 19:55:08 +0530

    Add Cart

--- FrontEnd/src/pages/Places/Places.tsx
+++ FrontEnd/src/pages/Places/Places.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header'
+import Card from '../../components/Card'
 
 export default function Places() {
   return (
     <>
     <Header/>
-    <div>Places</div>
+    <Card/>
     </>
   )
 }
