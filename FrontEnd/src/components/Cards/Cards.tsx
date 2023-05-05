b465f69aa320ee9e248ba9d6e88deb98cb4122dd
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 10:52:52 +0530

    Add Cart

--- FrontEnd/src/components/Cards/Cards.tsx
+++ FrontEnd/src/components/Cards/Cards.tsx
@@ -1,50 +1,43 @@
-
+import image1 from "../../assets/img/1.jpg";
 
 export default function Cards() {
   return (
-    <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img
-                  alt="Placeholder"
-                  className="block h-auto w-full"
-                  src="https://picsum.photos/600/400/?random"
-                />
-              </a>
+    <div className="my-1 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/4">
+      <article className="overflow-hidden rounded-lg shadow-lg">
+        <a href="#">
+          <img alt="Placeholder" className="block w-auto h-auto items-center"  src={image1} />
+        </a>
 
-              <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                <h1 className="text-lg">
-                  <a
-                    className="no-underline hover:underline text-black"
-                    href="#"
-                  >
-                    Article Title
-                  </a>
-                </h1>
-                <p className="text-grey-darker text-sm">11/1/19</p>
-              </header>
+        <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+          <h1 className="text-lg">
+            <a className="no-underline hover:underline text-black" href="#">
+              Lotues Tower
+            </a>
+          </h1>
+          <p className="text-grey-darker text-sm">Colombo</p>
+        </header>
 
-              <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-                <a
-                  className="flex items-center no-underline hover:underline text-black"
-                  href="#"
-                >
-                  <img
-                    alt="Placeholder"
-                    className="block rounded-full"
-                    src="https://picsum.photos/32/32/?random"
-                  />
-                  <p className="ml-2 text-sm">Author Name</p>
-                </a>
-                <a
-                  className="no-underline text-grey-darker hover:text-red-dark"
-                  href="#"
-                >
-                  <span className="hidden">Like</span>
-                  <i className="fa fa-heart"></i>
-                </a>
-              </footer>
-            </article>
-          </div>
+        <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+          <a
+            className="flex items-center no-underline hover:underline text-black"
+            href="#"
+          >
+            <img
+              alt="Placeholder"
+              className="block rounded-full"
+              src="https://picsum.photos/32/32/?random"
+            />
+            <p className="ml-4 text-sm">Author Name</p>
+          </a>
+          <a
+            className="no-underline text-grey-darker hover:text-red-dark"
+            href="#"
+          >
+            <span className="hidden">Like</span>
+            <i className="fa fa-heart"></i>
+          </a>
+        </footer>
+      </article>
+    </div>
   );
 }
--- FrontEnd/src/pages/Places/Places.tsx
+++ FrontEnd/src/pages/Places/Places.tsx
@@ -9,7 +9,9 @@ export default function Places() {
       <div className="container my-12 mx-auto px-4 md:px-12 mt-20">
         <div className="flex flex-wrap -mx-1 lg:-mx-4">
           <Cards />
-          
+          <Cards />
+          <Cards />
+          <Cards />
         </div>
       </div>
     </>
