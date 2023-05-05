e306298d10502f6f091575bf597934a535439e05
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 18:42:56 +0530

    Add new User Controller

--- BackEnd/src/controller/UserController.ts
+++ BackEnd/src/controller/UserController.ts
@@ -1,5 +1,10 @@
 import { RequestHandler, Request, Response } from "express";
 
 export default class UserController {
-  
+  addNewUser: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res;
+  };
 }
