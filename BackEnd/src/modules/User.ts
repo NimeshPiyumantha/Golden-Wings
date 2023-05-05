191bc386adac705a6658a3b090130f94bec57552
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 18:25:39 +0530

    update user module

--- BackEnd/src/modules/User.ts
+++ BackEnd/src/modules/User.ts
@@ -1,9 +1,42 @@
-export interface IUser extends Document{
-    roleId:string;
-    fristName:string;
-    lastName:string;
-    address:string;
-    contactNo:number;
-    email:string;
+import { Schema, model } from "mongoose";
+
+export interface IUser extends Document {
+  roleId: string;
+  fristName: string;
+  lastName: string;
+  address: string;
+  contactNo: number;
+  email: string;
 }
 
+const UserSchema = new Schema(
+  {
+    roleId: {
+      type: String,
+      required: true,
+    },
+    fristName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    contactNo: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model<IUser>("User", UserSchema);
