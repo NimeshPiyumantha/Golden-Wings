2da2afa89a1103230750c13b030882b642525a0d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 13:54:04 +0530

    Add Login

--- FrontEnd/src/pages/Login/Login.tsx
+++ FrontEnd/src/pages/Login/Login.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/material";
+import sigIn_bg from "../../assets/img/signIn-bg.jpg"
 
 export default function Login() {
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
@@ -10,5 +21,16 @@ export default function Login() {
     });
   };
 
-  return <div>Login</div>;
+
+  return (
+    <Container component="main" maxWidth="lg">
+    <Box
+      sx={{
+        marginTop: 8,
+      }}
+    >
+     
+    </Box>
+  </Container>
+  );
 }
