6abdaa5cae89be93eef59d1da9ca7be71d0f8f44
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 18:30:22 +0530

    Update role module

--- BackEnd/src/modules/Role.ts
+++ BackEnd/src/modules/Role.ts
@@ -1,3 +1,17 @@
-export interface IRole extends Document{
-    roleType:string;
+import { Schema, model } from "mongoose";
+
+export interface IRole extends Document {
+  roleType: string;
 }
+
+const RoleSchema = new Schema(
+  {
+    roleType: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Role = model<IRole>("Role", RoleSchema);
