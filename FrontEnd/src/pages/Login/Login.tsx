118352d9cd530af2b7e2369d9958d53e59b35a6a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 13:56:58 +0530

    update Login content

--- FrontEnd/src/pages/Login/Login.tsx
+++ FrontEnd/src/pages/Login/Login.tsx
@@ -48,7 +48,15 @@ export default function Login() {
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-         
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
            
         </Grid>
       </Grid>
