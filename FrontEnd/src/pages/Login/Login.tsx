92d56b9820e2338650a389f74e7d3b7c20ee0ce0
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-05 12:52:15 +0530

    Add Login

--- FrontEnd/src/pages/Login/Login.tsx
+++ FrontEnd/src/pages/Login/Login.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return <div>Login</div>;
 }
