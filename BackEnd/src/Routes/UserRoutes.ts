ab49ae6e667e933c946be30c3924c5d97fe8581c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-06 08:34:09 +0530

    Add UserRoutes

---
+++ BackEnd/src/Routes/UserRoutes.ts
@@ -0,0 +1,35 @@
+import express, { Router } from "express";
+import UserController from "../controller/UserController";
+
+export default class UserRoutes {
+  private router: Router = express.Router();
+  private routerController: UserController = new UserController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    // POST /api/v1/user
+    this.router.post("/", this.routerController.addNewUser);
+
+    // GET /api/v1/user
+    this.router.get("/", this.routerController.retrieveAllUser);
+
+    // PUT /api/v1/user/:id
+    this.router.put("/:id", this.routerController.updateUser);
+
+    // DELETE /api/v1/user/:id
+    this.router.delete("/:id", this.routerController.deleteUser);
+
+    // GET /api/v1/user?email=uEmail&password=uPassword
+    this.router.delete(
+      "?email=uEmail&password=uPassword",
+      this.routerController.signInUser
+    );
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
