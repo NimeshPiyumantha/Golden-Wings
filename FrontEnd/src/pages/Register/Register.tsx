86883ea1ec5176084101bff046c8e3d21588b84f
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-06 13:42:36 +0530

    Add Header section two  in register

--- FrontEnd/src/pages/Register/Register.tsx
+++ FrontEnd/src/pages/Register/Register.tsx
@@ -48,6 +48,40 @@ export default function Register() {
                   </div>
                 </div>
               </div>
+              <div className="flex -mx-3">
+                <div className="w-1/2 px-3 mb-5">
+                  <label form="" className="text-xs font-semibold px-1">
+                    Address <span className="text-accent-red-100">*</span>
+                  </label>
+                  <div className="flex">
+                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                      <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                    </div>
+                    <input
+                      type="text"
+                      className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                      placeholder="Colombo"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="w-1/2 px-3 mb-5">
+                  <label form="" className="text-xs font-semibold px-1">
+                    Contact <span className="text-accent-red-100">*</span>
+                  </label>
+                  <div className="flex">
+                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                      <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                    </div>
+                    <input
+                      type="number"
+                      className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                      placeholder="0777123456"
+                    />
+                  </div>
+                </div>
+              </div>
+              
              
             </div>
           </div>
