f47b4806e2d32f77de088bad302e2362025dd5dc
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-06 13:43:00 +0530

    Add Header section four in register

--- FrontEnd/src/pages/Register/Register.tsx
+++ FrontEnd/src/pages/Register/Register.tsx
@@ -113,7 +113,13 @@ export default function Register() {
                   </div>
                 </div>
               </div>
-             
+              <div className="flex -mx-3 mt-2">
+                <div className="w-full px-5 mb-5">
+                  <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+                    REGISTER NOW
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
