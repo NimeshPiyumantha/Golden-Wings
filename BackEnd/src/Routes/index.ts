79a2687b22fca67a59fb04d97624741c214d9ad9
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-06 08:42:01 +0530

    Update index.ts in routes

--- BackEnd/src/Routes/index.ts
+++ BackEnd/src/Routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import UserRoutes from "./UserRoutes";
+import RoleRoutes from "./RoleRoutes";
 
 const router: Router = Router();
 const url_prefix = "/api/v1";
 
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
+router.use(`${url_prefix}/role`, new RoleRoutes().getRouter());
 
 export default router;
