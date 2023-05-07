c655797eddbf5fc39a22f710cd56a583eee3d70b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:54:24 +0530

    Add PlaceRoutes link index

--- BackEnd/src/Routes/index.ts
+++ BackEnd/src/Routes/index.ts
@@ -3,6 +3,7 @@ import UserRoutes from "./UserRoutes";
 import RoleRoutes from "./RoleRoutes";
 import PostRoutes from "./PostRoutes ";
 import CategoryRoutes from "./CategoryRoutes";
+import PlaceRoutes from "./PlaceRoutes";
 
 const router: Router = Router();
 const url_prefix = "/api/v1";
@@ -11,5 +12,6 @@ router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
 router.use(`${url_prefix}/role`, new RoleRoutes().getRouter());
 router.use(`${url_prefix}/post`, new PostRoutes().getRouter());
 router.use(`${url_prefix}/category`, new CategoryRoutes().getRouter());
+router.use(`${url_prefix}/place`, new PlaceRoutes().getRouter());
 
 export default router;
