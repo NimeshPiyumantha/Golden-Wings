8a3f62c08fe0065e0510a61af507c14cc78b3425
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 11:27:52 +0530

    Add CategoryRoutes to index.ts in  routes

--- BackEnd/src/Routes/index.ts
+++ BackEnd/src/Routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import UserRoutes from "./UserRoutes";
 import RoleRoutes from "./RoleRoutes";
 import PostRoutes from "./PostRoutes ";
+import CategoryRoutes from "./CategoryRoutes";
 
 const router: Router = Router();
 const url_prefix = "/api/v1";
@@ -9,6 +10,6 @@ const url_prefix = "/api/v1";
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
 router.use(`${url_prefix}/role`, new RoleRoutes().getRouter());
 router.use(`${url_prefix}/post`, new PostRoutes().getRouter());
-
+router.use(`${url_prefix}/category`, new CategoryRoutes().getRouter());
 
 export default router;
