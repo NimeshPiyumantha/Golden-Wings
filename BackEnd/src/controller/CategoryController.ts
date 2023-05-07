80e2cd7acf414ec6e702de734440b4ddbb6d27a8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 10:47:33 +0530

    Add CategoryController to addNewCategory

--- BackEnd/src/controller/CategoryController.ts
+++ BackEnd/src/controller/CategoryController.ts
@@ -1,5 +1,12 @@
 import { RequestHandler, Request, Response } from "express";
 
 export default class CategoryController {
- 
+  addNewCategory: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res;
+  };
+
+
 }
