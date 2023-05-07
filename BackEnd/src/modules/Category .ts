b841f9b7fda2682b73cb0765da9d0ae9cf71f838
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 10:01:22 +0530

    Update Category  modules

--- BackEnd/src/modules/Category .ts
+++ BackEnd/src/modules/Category .ts
@@ -4,4 +4,14 @@ export interface ICategory extends Document {
   categoryName: string;
 }
 
+const CategorySchema = new Schema(
+  {
+    categoryName: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
 export const Category = model<ICategory>("Category", CategorySchema);
