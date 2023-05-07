c687dca1f4c04083b38153b68fae071bc2787b61
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:41:18 +0530

    Update PlaceController to Add new Place to POST code

--- BackEnd/src/controller/PlaceController.ts
+++ BackEnd/src/controller/PlaceController.ts
@@ -1,12 +1,108 @@
 import { RequestHandler, Request, Response } from "express";
 import { Place } from "../modules/Place";
+import mongoose, { ClientSession } from "mongoose";
+import { Category } from "../modules/Category ";
+import { Image } from "../modules/Image";
+import { Time } from "../modules/Time";
+import { Tag } from "../modules/Tag";
 
 export default class PlaceController {
   addNewPlace: RequestHandler = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    return res;
+    let session: ClientSession | null = null;
+
+    try {
+      const { categoryName } = req.body;
+      const { imageUrl } = req.body;
+      const { timeRange } = req.body;
+
+      // start the session and transaction
+      session = await mongoose.startSession();
+      session.startTransaction();
+
+      // check whether the relevant category already exists or not
+      let category = await Category.findOne({
+        categoryName: categoryName,
+      }).session(session);
+
+      if (!category) {
+        // save category only if not exists
+        category = new Category({ categoryName: categoryName });
+        category = await category.save();
+      }
+
+      // check whether the relevant images already exists or not
+      let images = await Image.findOne({
+        imageUrl: imageUrl,
+      }).session(session);
+
+      if (!images) {
+        // save image only if not exists
+        images = new Image({ imageUrl: imageUrl });
+        images = await images.save();
+      }
+
+      // check whether the relevant time Range already exists or not
+      let time = await Time.findOne({
+        timeRange: timeRange,
+      }).session(session);
+
+      if (!time) {
+        // save time Range only if not exists
+        time = new Time({ timeRange: timeRange });
+        time = await time.save();
+      }
+
+      const post = new Place(req.body);
+
+      // set the category id here
+      post.categoryId = category._id.toString();
+      // set the image id here
+      post.imageId = images._id.toString();
+      // set the time id here
+      post.timeId = time._id.toString();
+
+      // save Post details
+      let newPost = await post.save();
+
+      // getting the tags array from request body
+      const tags = req.body.tags;
+
+      // save tags
+      if (tags.length > 0) {
+        for (let i = 0; i < tags.length; i++) {
+          // check whether the relevant tag already exists or not
+          let tag = await Tag.findOne({ text: tags[i] }).session(session);
+          if (!tag) {
+            tag = new Tag({ text: tags[i] });
+            await tag.save();
+          }
+        }
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return res
+        .status(200)
+        .json({ message: "New Post created.", responseData: newPost });
+    } catch (error: unknown) {
+      if (session != null) {
+        try {
+          await session.abortTransaction();
+        } catch (abortError) {
+          console.log(`Error aborting transaction: ${abortError}`);
+        }
+      }
+
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
   };
 
   updatePlace: RequestHandler = async (
