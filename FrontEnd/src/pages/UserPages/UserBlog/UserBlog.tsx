f29bcc5444054e29e3dfb9b98443f90711753faa
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 20:43:28 +0530

    Add Blog section crate post

--- FrontEnd/src/pages/UserPages/UserBlog/UserBlog.tsx
+++ FrontEnd/src/pages/UserPages/UserBlog/UserBlog.tsx
@@ -1,11 +1,21 @@
 import UsersHeader from "../../../components/UsersHeader";
 import Footer from "../../../components/Footer";
+import { TextField } from "@mui/material";
 
 export default function UserBlog() {
   return (
     <>
       <UsersHeader />
-      <h1 className="mt-20">Hi</h1>
+
+      <div className="mt-24">
+        <div className="flex justify-center">
+          <div className="bg-accent-white-50 shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+          <h1 className="text-center pb-6 font-Ubuntu font-bold text-lg">Create New Post</h1>
+          
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </>
   );
