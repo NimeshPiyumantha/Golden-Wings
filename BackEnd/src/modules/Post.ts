5a535070d9a3948083fd5e718074dfcb525fed78
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 09:48:53 +0530

    Update Post modules

--- BackEnd/src/modules/Post.ts
+++ BackEnd/src/modules/Post.ts
@@ -1,9 +1,42 @@
+import { Document, Schema } from "mongoose";
+
 export interface IPost extends Document {
   userId: string;
   images: string[];
-  date: string;
+  date: Date;
   title: string;
   description: string;
   tags: string[];
   categoryId: string;
 }
+
+const PostSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: Array,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: Array,
+    required: true,
+  },
+  categoryId: {
+    type: String,
+    required: true,
+  },
+});
