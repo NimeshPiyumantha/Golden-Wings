bef52c087128377436c49586a6c3c130fb11782c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 09:58:30 +0530

    Update Image  modules

--- BackEnd/src/modules/Image.ts
+++ BackEnd/src/modules/Image.ts
@@ -4,6 +4,15 @@ export interface IImage extends Document {
   image: string;
 }
 
-
+// creating schema
+const ImageSchema = new Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Image = model<IImage>("Image", ImageSchema);
