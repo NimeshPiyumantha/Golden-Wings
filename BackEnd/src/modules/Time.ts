3834ef92a7e61b8ee10281eb582006f09b284c26
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:20:57 +0530

    Add Time module schema

--- BackEnd/src/modules/Time.ts
+++ BackEnd/src/modules/Time.ts
@@ -4,6 +4,15 @@ export interface ITime extends Document {
   timeId: string;
 }
 
-
+// creating schema
+const TimeSchema = new Schema(
+  {
+    timeId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Time = model<ITime>("Time", TimeSchema);
