10516ad06730e73f24f0cf7405843ca515c04b5c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 11:27:31 +0530

    Add PostRoutes to index.ts in  routes

--- BackEnd/src/Routes/index.ts
+++ BackEnd/src/Routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import UserRoutes from "./UserRoutes";
 import RoleRoutes from "./RoleRoutes";
+import PostRoutes from "./PostRoutes ";
 
 const router: Router = Router();
 const url_prefix = "/api/v1";
 
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter());
 router.use(`${url_prefix}/role`, new RoleRoutes().getRouter());
+router.use(`${url_prefix}/post`, new PostRoutes().getRouter());
+
 
 export default router;
