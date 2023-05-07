e432bb274b3f065ba16cb7211c96d3d911de3908
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 20:43:52 +0530

    Add Blog section crate post section 1

--- FrontEnd/src/pages/UserPages/UserBlog/UserBlog.tsx
+++ FrontEnd/src/pages/UserPages/UserBlog/UserBlog.tsx
@@ -11,7 +11,31 @@ export default function UserBlog() {
         <div className="flex justify-center">
           <div className="bg-accent-white-50 shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
           <h1 className="text-center pb-6 font-Ubuntu font-bold text-lg">Create New Post</h1>
-          
+            <div className="-mx-3 md:flex mb-4">
+              <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+                <TextField
+                  type="file"
+                  variant="outlined"
+                  name="imageUrl"
+                  // onChange={this.handleInputChange}
+                  // value={this.state.imageUrl}
+                  fullWidth={true}
+                  required
+                />
+              </div>
+              <div className="md:w-1/2 px-3">
+                <TextField
+                  type="date"
+                  variant="outlined"
+                  name="date"
+                  // onChange={this.handleInputChange}
+                  // value={this.state.date}
+                  fullWidth={true}
+                  required
+                />
+              </div>
+            </div>
+           
           </div>
         </div>
       </div>
