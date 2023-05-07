6d61e3964d79782f8c47186dc74559e4e2f16542
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:29:30 +0530

    Update PlaceController to retrieveAllPlace to GET code

--- BackEnd/src/controller/PlaceController.ts
+++ BackEnd/src/controller/PlaceController.ts
@@ -1,4 +1,5 @@
 import { RequestHandler, Request, Response } from "express";
+import { Place } from "../modules/Place";
 
 export default class PlaceController {
   addNewPlace: RequestHandler = async (
@@ -26,6 +27,15 @@ export default class PlaceController {
     req: Request,
     res: Response
   ): Promise<Response> => {
-    return res;
+    try {
+      const place = await Place.find();
+      return res.status(200).json({ responseData: place });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
   };
 }
