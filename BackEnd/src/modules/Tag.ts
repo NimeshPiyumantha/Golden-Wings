e61fd186b9dae4392c29ff8f571d88820dc4464c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 09:53:26 +0530

    update Tag  modules

--- BackEnd/src/modules/Tag.ts
+++ BackEnd/src/modules/Tag.ts
@@ -4,6 +4,15 @@ export interface ITag extends Document {
   text: string;
 }
 
-
+// creating schema
+const TagSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Tag = model<ITag>("Tag", TagSchema);
