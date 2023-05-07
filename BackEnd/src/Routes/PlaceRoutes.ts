349d3a004673295d169847914f2db56b5c89b597
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:52:21 +0530

    Add PlaceRoutes post mapping

--- BackEnd/src/Routes/PlaceRoutes.ts
+++ BackEnd/src/Routes/PlaceRoutes.ts
@@ -1,15 +1,18 @@
 import express, { Router } from "express";
-
+import PlaceController from "../controller/PlaceController";
 
 export default class PlaceRoutes {
   private router: Router = express.Router();
-
+  private placeController: PlaceController = new PlaceController();
 
   constructor() {
     this.configRoutes();
   }
 
   private configRoutes = (): void => {
+    // POST /api/v1/place
+    this.router.post("/", this.placeController.addNewPlace);
+
 
   };
 
