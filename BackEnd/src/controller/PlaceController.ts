9f65b50779e45ca2fa697978fd31c05b0651a036
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:25:36 +0530

    Add PlaceController to deletePlace

--- BackEnd/src/controller/PlaceController.ts
+++ BackEnd/src/controller/PlaceController.ts
@@ -15,7 +15,12 @@ export default class PlaceController {
     return res;
   };
 
-
+  deletePlace: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res;
+  };
 
 
 }
