26e33690dd5f7c27a60bab03ec68e7d6812e1045
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 16:25:06 +0530

    Add PlaceController to addNewPlace

--- BackEnd/src/controller/PlaceController.ts
+++ BackEnd/src/controller/PlaceController.ts
@@ -1,5 +1,16 @@
 import { RequestHandler, Request, Response } from "express";
 
 export default class PlaceController {
+  addNewPlace: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res;
+  };
+
+
+
+
+
 
 }
