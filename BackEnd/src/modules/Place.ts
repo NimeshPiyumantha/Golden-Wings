179e89c65197565d0d2b3c0ba7e6bc7b72fc0568
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-07 15:46:38 +0530

    Add Place model to schema

--- BackEnd/src/modules/Place.ts
+++ BackEnd/src/modules/Place.ts
@@ -5,12 +5,53 @@ export interface IPlace extends Document {
   description: string;
   images: string;
   date: Date;
-  cost:number;
-  contact:number;
-  time:string,
+  time: string;
+  cost: number;
+  contact: number;
   tags: string[];
   categoryId: string;
 }
 
-
+// creating schema
+const PlaceSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    cost: {
+      type: Number,
+      required: true,
+    },
+    contact: {
+      type: Number,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      required: true,
+    },
+    categoryId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 export const Place = model<IPlace>("Place", PlaceSchema);
