10fa826e351598ef239d2c18899033208c69df40
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-09 11:29:32 +0530

    Update Content

--- FrontEnd/src/pages/AdminPages/PostManage/PostManage..tsx
+++ FrontEnd/src/pages/AdminPages/PostManage/PostManage..tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import AdminHeader from "../../../components/AdminHeader";
+import Footer from "../../../components/Footer";
 
 export default function PostManage() {
   return (
-    <div>PostManage</div>
-  )
+    <>
+      <AdminHeader />
+      <div className="mt-22">Post Mange</div>
+      <Footer />
+    </>
+  );
 }
