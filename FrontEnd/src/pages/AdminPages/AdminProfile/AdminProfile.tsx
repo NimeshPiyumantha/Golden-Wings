f86c8f11c229d9e4da65a8db5a7139a0c0c5cd8a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-09 11:39:57 +0530

    Update Content

---
+++ FrontEnd/src/pages/AdminPages/AdminProfile/AdminProfile.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminHeader from "../../../components/AdminHeader";
+import Footer from "../../../components/Footer";
+import Profile from "../../../components/Profile";
+
+export default function AdminProfile() {
+  return (
+    <>
+      <AdminHeader />
+      <Profile />
+      <Footer />
+    </>
+  );
+}
