f6a5da7a6b181426784047eb9259a9ca48215783
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-12 01:32:31 +0530

    update content

--- FrontEnd/src/components/Cards/Cards.tsx
+++ FrontEnd/src/components/Cards/Cards.tsx
@@ -20,38 +20,23 @@ export default function Cards(props: PlaceDetails) {
     <div className="my-1 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/4">
       <article className="overflow-hidden rounded-lg shadow-lg">
         <a href="#">
-          <img alt="Placeholder" className="block w-auto h-auto items-center"  src={image1} />
+          <img
+            alt="Placeholder"
+            className="block w-auto h-auto items-center"
+            src={image1}
+          />
         </a>
 
         <header className="flex items-center justify-between leading-tight p-2 md:p-4">
           <h1 className="text-lg">
             <a className="no-underline hover:underline text-black" href="#">
-              Lotues Tower
+              {props.title}
             </a>
           </h1>
-          <p className="text-grey-darker text-sm">Colombo</p>
+          <p className="text-grey-darker text-sm">{props.location}</p>
         </header>
 
-        <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-          <a
-            className="flex items-center no-underline hover:underline text-black"
-            href="#"
-          >
-            <img
-              alt="Placeholder"
-              className="block rounded-full"
-              src="https://picsum.photos/32/32/?random"
-            />
-            <p className="ml-4 text-sm">Author Name</p>
-          </a>
-          <a
-            className="no-underline text-grey-darker hover:text-red-dark"
-            href="#"
-          >
-            <span className="hidden">Like</span>
-            <i className="fa fa-heart"></i>
-          </a>
-        </footer>
+        <p className="m-4 mx-4 text-sm">{props.description}</p>
       </article>
     </div>
   );
