1ccaeef45bd9a7765875f296c3029914a68c101a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-11 16:30:46 +0530

    update content

--- FrontEnd/src/pages/AdminPages/PostManage/PostManage..tsx
+++ FrontEnd/src/pages/AdminPages/PostManage/PostManage..tsx
@@ -259,7 +259,154 @@ export default function PostManage() {
             </div>
           </div>
         </div>
-       
+        <div className="mt-3 px-1 m-3 md:px-2 sm:px-1 lg:px-20">
+          <div>
+            <div className="lg:w-2/5 sm:w-1/2 md:w-4/5">
+              <div className="my-5 mx-2">
+                <div className="m-4">
+                  <TextField
+                    label="Search"
+                    name="searchKey"
+                    variant="filled"
+                    fullWidth
+                    value={searchKey}
+                    onChange={(e) => setSearchKey(e.currentTarget.value)}
+                    onKeyDown={handleKeyDown}
+                    sx={{ mx: 2, borderRadius: "15px" }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <SearchIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <TableContainer component={Paper}>
+              <Table
+                style={{ backgroundColor: "#f5f5f5", borderRadius: "20px" }}
+              >
+                <TableHead style={{ backgroundColor: "#bdc3c7" }}>
+                  <TableRow>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Title
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Cost
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Date
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Category
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Contact No
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Duration
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Image Id
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Description
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      style={{
+                        fontWeight: "bolder",
+                        fontFamily: "Poppins",
+                        fontSize: "16px",
+                      }}
+                    >
+                      Tags
+                    </TableCell>
+                    <TableCell align="center"></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {/* {filteredData.map((row, index) => (
+                  <TableRow key={row._id}>
+                    <TableCell align="center">{row.fristName}</TableCell>
+                    <TableCell align="center">{row.lastName}</TableCell>
+                    <TableCell align="center">{row.address}</TableCell>
+                    <TableCell align="center">{row.contactNo}</TableCell>
+                    <TableCell align="center">{row.email}</TableCell>
+                    <TableCell align="center">{row.password}</TableCell>
+                    <TableCell align="center">
+                      <IconButton
+                        onClick={() => handleDeleteSelectedRows(row._id)}
+                      >
+                        <DeleteIcon sx={{ color: "red" }} />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))} */}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
