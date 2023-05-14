590b0fed054b66134bd9f7c7d389ac77cb8b0b33
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-14 10:58:07 +0530

    update content

--- BackEnd/src/Routes/index.ts
+++ BackEnd/src/Routes/index.ts
@@ -4,6 +4,7 @@ import RoleRoutes from "./RoleRoutes";
 import PostRoutes from "./PostRoutes ";
 import CategoryRoutes from "./CategoryRoutes";
 import PlaceRoutes from "./PlaceRoutes";
+import ImageRoutes from "./ImageRoutes";
 
 const router: Router = Router();
 const url_prefix = "/api/v1";
@@ -13,5 +14,6 @@ router.use(`${url_prefix}/role`, new RoleRoutes().getRouter());
 router.use(`${url_prefix}/post`, new PostRoutes().getRouter());
 router.use(`${url_prefix}/category`, new CategoryRoutes().getRouter());
 router.use(`${url_prefix}/place`, new PlaceRoutes().getRouter());
+router.use(`${url_prefix}/image`, new ImageRoutes().getRouter());
 
 export default router;
