1658e70554a3a3615f0f4959f24816a73a995b12
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-14 10:53:04 +0530

    Add image controller

--- BackEnd/src/controller/ImageController.ts
+++ BackEnd/src/controller/ImageController.ts
@@ -1,3 +1,20 @@
+import { RequestHandler, Request, Response } from "express";
+import { Image } from "../modules/Image";
+
 export default class ImageController {
-    
+  retrieveAllImages: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      let image = await Image.find();
+      return res.status(200).json({ responseData: image });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Unknown error occured." });
+      }
+    }
+  };
 }
