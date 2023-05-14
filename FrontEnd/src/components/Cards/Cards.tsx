9f6aee32dc7702d3d57b506556b337c0e7d151df
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-15 02:20:48 +0530

    update content

--- FrontEnd/src/components/Cards/Cards.tsx
+++ FrontEnd/src/components/Cards/Cards.tsx
@@ -17,15 +17,21 @@ export default function Cards(props: PlaceDetails) {
   return (
     <div className="my-1 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/4">
       <article className="overflow-hidden rounded-lg shadow-lg border-2">
-      <h3 className="text-accent-gray-200 hover:text-primary flex justify-center p-2 text-lg font-semibold sm:text-1xl lg:text-xl xl:text-1xl">
-            {props.categoryName}
-          </h3>
-      <div className="mb-4 overflow-hidden rounded w-60 flex flex-row-reverse">
+        <h3 className="text-accent-gray-200 hover:text-primary flex justify-center p-2 text-lg font-semibold sm:text-1xl lg:text-xl xl:text-1xl">
+          {props.title}
+        </h3>
+        <div className="mb-4 overflow-hidden rounded w-60 flex flex-row-reverse">
           <img src={props.imageUrl} alt="image" className="w-3/4" />
         </div>
-
+        <div className="m-5 mx-5 flex flex-col space-y-1">
+        <h3 className="text-accent-green-50 font-Poppins font-bold text-sm">Location : <span className="text-gray-500 ml-1"> {props.location}</span></h3>
+        <h3 className="text-accent-green-50 font-Poppins font-bold text-sm">Cost : <span className="text-gray-500 ml-1"> {props.cost}</span></h3>
+        <h3 className="text-accent-green-50 font-Poppins font-bold text-sm">Contact : <span className="text-gray-500 ml-1"> {props.contact}</span></h3>
+        <h3 className="text-accent-green-50 font-Poppins font-bold text-sm">Time Range : <span className="text-gray-500 ml-1"> {props.timeRange}</span></h3>
+        <h3 className="text-accent-green-50 font-Poppins font-bold text-sm">Category : <span className="text-gray-500 ml-1"> {props.categoryName}</span></h3>
+        
+        </div>
        
-        <p className="m-3 mx-4 text-sm">{props.description}</p>
       </article>
     </div>
   );
