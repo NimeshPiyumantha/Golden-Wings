e15e9538dc145c6cd6c53327f1abbe070377614a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-17 10:35:25 +0530

    Update Content

--- BackEnd/src/Routes/PlaceRoutes.ts
+++ BackEnd/src/Routes/PlaceRoutes.ts
@@ -16,8 +16,8 @@ export default class PlaceRoutes {
     // GET /api/v1/place
     this.router.get("/", this.placeController.retrieveAllPlace);
 
-     // GET /api/v1/place/:id
-     this.router.get("/search/:id", this.placeController.searchPlace);
+    // GET /api/v1/place/:id
+    this.router.get("/search/:id", this.placeController.searchPlace);
 
     // PUT /api/v1/place/:id
     this.router.put("/:id", this.placeController.updatePlace);
