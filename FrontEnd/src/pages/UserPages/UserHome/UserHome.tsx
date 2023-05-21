d86f90ad2f6f90435478f870c73238e92ca743b3
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-05-21 09:58:29 +0530

    Update Content

--- FrontEnd/src/pages/UserPages/UserHome/UserHome.tsx
+++ FrontEnd/src/pages/UserPages/UserHome/UserHome.tsx
@@ -2,12 +2,14 @@ import UsersHeader from "../../../components/UsersHeader";
 import Collection from "../../Gallery/Collection";
 import Footer from "../../../components/Footer";
 import UserHero from "./UserHero";
+import PlacesCard from "../../../components/PlacesCard";
 
 export default function UserHome() {
   return (
     <>
       <UsersHeader />
       <UserHero />
+      <PlacesCard />
       <Collection />
       <Footer />
     </>
